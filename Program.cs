4343ffa68a6cf2b3be471a6e2cbcf3a1d63d20d6
Sharaf-Mansour <sharaf_1015@yahoo.com>
2023-01-08 08:04:24 +0200

    Bug Fixes and Performance improvement

--- Program.cs
+++ Program.cs
@@ -1,36 +1,34 @@
-﻿using Newtonsoft.Json;
-using PoniLCU;
-using System;
+﻿using PoniLCU;
+using System.Text.Json;
+using LOLFriendsCleaner;
 using static PoniLCU.LeagueClient;
 
-namespace LOLFriendsCleaner
+var leagueClient = new LeagueClient(credentials.cmd);
+
+var data = await leagueClient.Request(requestMethod.GET, "/lol-chat/v1/friends");
+var friends = JsonSerializer.Deserialize<List<Friend>>(data);
+
+Console.WriteLine($$"""
+                    Are you sure you want to delete all of your friends?
+                    You have {{friends?.Count ?? 0}} friends in game!
+                    Type 1 for yes, 2 for no
+                    """);
+
+var input = int.Parse(Console.ReadLine() ?? "0");
+if ((input, friends) is (1, { }))
 {
-    public class Program
+    foreach (var friend in friends)
     {
-        static LeagueClient leagueClient = new LeagueClient(credentials.cmd);
-        static async Task Main(string[] args)
-        {
-            var data = await leagueClient.Request(requestMethod.GET, "/lol-chat/v1/friends");
-            var friends = JsonConvert.DeserializeObject<List<Friend>>(data);
-            Console.WriteLine("Are you sure you want to delete all of your friends?");
-            Console.WriteLine($"You have {friends.Count} friends in game!");
-            Console.WriteLine("Type 1 for yes, 2 for no");
-            int input = Convert.ToInt32(Console.ReadLine());
-            if (input == 1)
-            {
-                foreach (var friend in friends)
-                {
-                    await leagueClient.Request(requestMethod.DELETE, $"/lol-chat/v1/friends/{friend.puuid}");
-                    Console.WriteLine($"Removed {friend.gameName}");
-                }
-                Console.WriteLine("Removed All friends!");
-                Console.WriteLine("Please restart your game to make sure that It's not bugged!");
-            }
-            else
-            {
-                Console.WriteLine("Exiting...");
-                Thread.Sleep(1000);
-            }
-        }
+        await leagueClient.Request(requestMethod.DELETE, $"/lol-chat/v1/friends/{friend.puuid}");
+        Console.WriteLine($"Removed {friend.gameName}");
     }
+    Console.WriteLine($$"""
+                        Removed All friends!
+                        Please restart your game to make sure that It's not bugged!
+                        """);
+}
+else
+{
+    Console.WriteLine("Exiting...");
+    Thread.Sleep(1000);
 }
