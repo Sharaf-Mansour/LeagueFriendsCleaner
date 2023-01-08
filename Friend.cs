8cc666083206b61be94dd8f66ad6c798e78c2dae
Sharaf-Mansour <sharaf_1015@yahoo.com>
2023-01-08 08:03:24 +0200

    COERUB

--- Friend.cs
+++ Friend.cs
@@ -1,29 +1,27 @@
-﻿namespace LOLFriendsCleaner
+﻿namespace LOLFriendsCleaner;
+public class Friend
 {
-    public class Friend
-    {
-        public string? availability { get; set; }
-        public long displayGroupId { get; set; }
-        public string? displayGroupName { get; set; }
-        public string? gameName { get; set; }
-        public string? gameTag { get; set; }
-        public long groupId { get; set; }
-        public string? groupName { get; set; }
-        public long icon { get; set; }
-        public string? id { get; set; }
-        public bool isP2PConversationMuted { get; set; }
-        public string? lastSeenOnlineTimestamp { get; set; }
-        public string? name { get; set; }
-        public string? note { get; set; }
-        public string? patchline { get; set; }
-        public string? pid { get; set; }
-        public string? platformId { get; set; }
-        public string? product { get; set; }
-        public string? productName { get; set; }
-        public string? puuid { get; set; }
-        public string? statusMessage { get; set; }
-        public string? summary { get; set; }
-        public long summonerId { get; set; }
-        public long time { get; set; }
-    }
+    public string? availability { get; set; }
+    public long displayGroupId { get; set; }
+    public string? displayGroupName { get; set; }
+    public string? gameName { get; set; }
+    public string? gameTag { get; set; }
+    public long groupId { get; set; }
+    public string? groupName { get; set; }
+    public long icon { get; set; }
+    public string? id { get; set; }
+    public bool isP2PConversationMuted { get; set; }
+    public string? lastSeenOnlineTimestamp { get; set; }
+    public string? name { get; set; }
+    public string? note { get; set; }
+    public string? patchline { get; set; }
+    public string? pid { get; set; }
+    public string? platformId { get; set; }
+    public string? product { get; set; }
+    public string? productName { get; set; }
+    public string? puuid { get; set; }
+    public string? statusMessage { get; set; }
+    public string? summary { get; set; }
+    public long summonerId { get; set; }
+    public long time { get; set; }
 }
